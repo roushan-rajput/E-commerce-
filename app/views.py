4da66b74a2b028d506809ad50617ca5b95c32d63
Roushan Kumar <roushanrajput12362@gmail.com>
2026-07-27 15:08:43 +0530

    Adding Add to card and EDIT & DELETE button swipup Logic throgh their specific Login Users

--- app/views.py
+++ app/views.py
@@ -8,94 +8,49 @@ def landing(req):
 
 def login(req):
     if req.method == 'POST':
-
         e = req.POST.get('email')
-
         p = req.POST.get('password')
-
-
         # ADMIN LOGIN
-
         if e == 'admin@gmail.com' and p == 'admin':
-
             req.session["admin"] = True
-
             return redirect(
                 'admindash'
             )
-
-
         user = User.objects.filter(
-
             useremail=e
-
         )
-
-
         if not user.exists():
-
             message = 'User does not exist'
-
             print(user)
-
             return render(
-
                 req,
-
                 'login.html',
-
                 {
-
                     'error': message
-
                 }
-
             )
-
-
         else:
-
             user_data = User.objects.get(
-
                 useremail=e
-
             )
-
-
             if p == user_data.userpassword:
-
                 req.session["user_id"] = user_data.id
-
                 return redirect(
                     'dashboard'
                 )
-
-
             else:
-
                 message = 'Incorrect password'
-
                 return render(
-
                     req,
-
                     'login.html',
-
                     {
-
                         'error': message
-
                     }
-
                 )
-
-
     return render(
-
         req,
 
         'login.html'
-
     )
 
 
@@ -124,6 +79,7 @@ def admindash(req):
         return redirect("login")
     return render(req, "admindash.html")
 
+
 def dashboard(req):
 
     user_id = req.session.get("user_id")
@@ -147,6 +103,7 @@ def dashboard(req):
 def logout(req):
     req.session.flush()
     return redirect("login")
+
 
 def addpro(req):
     if req.method == 'POST':
@@ -180,4 +137,3 @@ def allpro(req):
             "is_admin": is_admin
         }
     )
-    
